c94e8b5b7f28abe4c7f499077cf2d1ef8156d670
Queen Umesi <queenumesi01@gmail.com>
2025-06-25 16:00:42 +0400

    feat: update CourseCard components with dynamic course data and improved styling

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-import { ShoppingCartIcon, MagnifyingGlassIcon, ArrowPathRoundedSquareIcon } from "@heroicons/vue/24/solid";
+import { ref } from "vue";
+import { ShoppingCartIcon, MagnifyingGlassIcon, ArrowPathRoundedSquareIcon, XMarkIcon, TrashIcon } from "@heroicons/vue/24/solid";
 import CourseCard from "./components/CourseCard.vue";
+import courses from "./data.json";
 </script>
 
 <template>
-	<header class="flex justify-between items-center bg-amber-900 text-white p-6 sticky top-0 w-full z-10">
+	<header class="flex justify-between items-center bg-cyan-900 text-white p-6 sticky top-0 w-full z-10">
 		<h1 class="font-kaushan text-4xl font-semibold cursor-default text-shadow-lg">Online Course Shop</h1>
 		<div class="flex items-center gap-3 font-space-grotesk">
 			<p class="text-lg cursor-default">$0.0</p>
 			<button class="relative rounded-full p-2 hover:bg-white/20 transition duration-200 cursor-pointer peer">
 				<ShoppingCartIcon class="size-7" />
 				<p
-					class="absolute top-1 text-xs right-0 font-bold text-amber-700 bg-white rounded-full w-4 outline-1 outline-amber-70 data-[disabled=true]:outline-gray-400 ">
+					class="absolute top-1 text-xs right-0 font-bold text-cyan-700 bg-white rounded-full w-4 outline-1 outline-cyan-70 data-[disabled=true]:outline-gray-400 ">
 					0
 				</p>
 			</button>
@@ -21,52 +23,52 @@ import CourseCard from "./components/CourseCard.vue";
 			</span>
 		</div>
 	</header>
-	<main class="bg-amber-50/30 font-space-grotesk">
+	<main class="bg-cyan-50/30 font-space-grotesk">
 		<div class="flex w-full p-20 gap-10 divide-x divide-gray-300">
 			<div class="basis-1/4 ">
 				<div class="flex items-center gap-1 h-10 mb-10 w-[calc(100%-3.5rem)]">
 					<input type="text" placeholder="Search for courses"
 						class=" p-2 border border-gray-300 rounded-l-md h-full w-full" />
 					<button
-						class="bg-amber-600 rounded-r-md h-full w-12 inline-flex items-center justify-center hover:bg-amber-600/80  cursor-pointer transition duration-200">
+						class="bg-cyan-600 rounded-r-md h-full w-12 inline-flex items-center justify-center hover:bg-cyan-600/70  cursor-pointer transition duration-200">
 						<MagnifyingGlassIcon class="size-5 text-white" />
 					</button>
 				</div>
 				<div class="flex flex-col gap-5">
 					<h2 class="text-xl font-semibold">Filters</h2>
 					<ul class="*:flex *:gap-2 *:w-fit">
-						<li class="hover:text-amber-500 transition duration-200 *:accent-amber-700">
+						<li class="hover:text-cyan-500 transition duration-200 *:accent-cyan-700">
 							<input type="radio" id="subject" name="filter" value="subject" />
 							<label for="subject">Subject</label>
 						</li>
-						<li class="hover:text-amber-500 transition duration-200 *:accent-amber-700">
+						<li class="hover:text-cyan-500 transition duration-200 *:accent-cyan-700">
 							<input type="radio" id="location" name="filter" value="location" />
 							<label for="location">Location</label>
 						</li>
-						<li class="hover:text-amber-500 transition duration-200 *:accent-amber-700">
+						<li class="hover:text-cyan-500 transition duration-200 *:accent-cyan-700">
 							<input type="radio" id="price" name="filter" value="price" />
 							<label for="price">Price</label>
 						</li>
-						<li class="hover:text-amber-500 transition duration-200 *:accent-amber-700">
+						<li class="hover:text-cyan-500 transition duration-200 *:accent-cyan-700">
 							<input type="radio" id="available" name="filter" value="available" />
 							<label for="available">Availability</label>
 						</li>
 					</ul>
 					<div>
 						<h3 class="text-md font-medium mb-2"> Sort by </h3>
-						<ul class="*:flex *:gap-2 pl-4">
-							<li class="hover:text-amber-500 transition duration-200 *:accent-amber-700">
+						<ul class="*:flex *:gap-2 w-fit pl-4">
+							<li class="hover:text-cyan-500 transition duration-200 *:accent-cyan-700">
 								<input type="radio" id="ascending" name="sort" value="ascending" checked />
 								<label for="ascending">Ascending</label>
 							</li>
-							<li class="hover:text-amber-500 transition duration-200 *:accent-amber-700">
+							<li class="hover:text-cyan-500 transition duration-200 *:accent-cyan-700">
 								<input type="radio" id="descending" name="sort" value="descending" />
 								<label for="descending">Descending</label>
 							</li>
 						</ul>
 					</div>
 					<button
-						class="bg-amber-600 text-white px-4 py-2 rounded-md w-[calc(100%-3.5rem)] cursor-pointer hover:bg-amber-600/80 transition duration-200">
+						class="bg-cyan-600 text-white px-4 py-2 rounded-md w-[calc(100%-3.5rem)] cursor-pointer hover:bg-cyan-600/70 transition duration-200">
 						Apply
 					</button>
 				</div>
@@ -75,26 +77,20 @@ import CourseCard from "./components/CourseCard.vue";
 				<div class="flex items-end justify-between mb-10">
 					<h2 class="text-2xl font-semibold">All Courses</h2>
 					<button
-						class="text-base hover:underline hover:text-amber-500 transition duration-200 cursor-pointer">
+						class="text-base hover:underline hover:text-cyan-500 transition duration-200 cursor-pointer">
 						Clear filters
 					</button>
 				</div>
-				<div class=" grid grid-cols-1 md:grid-cols-3 gap-7 w-full">
-					<CourseCard />
-					<CourseCard />
-					<CourseCard />
-					<CourseCard />
-					<CourseCard />
-					<CourseCard />
-					<CourseCard />
-					<CourseCard />
-					<CourseCard />
-					<CourseCard />
-					<CourseCard />
+				<div
+					class="grid grid-cols-1 md:grid-cols-3 gap-5 w-full overflow-y-auto max-h-[calc(100vh-112px)] p-1 pb-5">
+					<CourseCard v-for="(course) in courses" :key="course.id" :id="course.id" :imageUrl="course.imageUrl"
+						:title="course.name" :location="course.location" :price="course.price" :slots="course.inStock"
+						:description="course.description" />
 				</div>
 			</div>
 		</div>
 	</main>
+
 </template>
 
 <style scoped></style>
--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -1,26 +1,40 @@
 <script setup lang="ts">
-import { ShoppingBagIcon } from "@heroicons/vue/24/solid";
+import { ShoppingBagIcon, MapPinIcon } from "@heroicons/vue/24/solid";
+import { defineProps } from "vue";
+
+const props = defineProps({
+    id: String,
+    title: String,
+    imageUrl: String,
+    location: String,
+    price: [String, Number],
+    slots: [String, Number],
+    description: String
+});
 </script>
 
 <template>
-    <div class=" bg-white font-space-grotesk flex flex-col overflow-hidden">
-        <div class=" bg-amber-600 w-full h-56 relative group">
+    <div
+        class=" bg-white font-space-grotesk flex flex-col overflow-hidden shadow-md rounded-lg hover:shadow-lg transition-shadow duration-200">
+        <div class="w-full h-52 relative group border-b border-gray-100">
+            <img :src="imageUrl" alt="Course Image"
+                class="w-full h-full object-cover object-left-top group-hover:scale-105 transition-transform duration-200" />
             <button
-                class="bg-white/20 rounded-full p-2 absolute right-2 top-2 hover:bg-white/50 transition duration-200 cursor-pointer peer hidden group-hover:block">
+                class="bg-white/80 rounded-full p-2 absolute right-2 top-2 hover:bg-white transition-all duration-200 cursor-pointer peer hidden group-hover:block drop-shadow-xl/50">
                 <ShoppingBagIcon class="size-6 text-gray-950/80" />
             </button>
-            <span role="tooltip"
-                class="text-white bg-gray-950/80 p-1 px-2 text-xs rounded-full hidden peer-hover:block absolute top-7 right-10 transition-all duration-200 ease-in-out delay-75">
+            <span role="tooltip" class="text-white bg-gray-950/80 p-1 px-2 text-xs rounded-full hidden peer-hover:block  absolute top-7 right-10 transition-all duration-200 ease-in-out delay-75 drop-shadow-lg">
                 Add to cart
             </span>
         </div>
-        <div class="space-y-4 p-2">
-            <h2 class="font-bold text-xl">Course Title</h2>
-            <p class="text-justify ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium aspernatur ...
+        <div class="p-4 h-72 flex flex-col gap-2">
+            <h2 class="font-bold text-lg">{{ title }}</h2>
+            <p class="text-sm text-cyan-500 inline-flex items-center gap-1">
+                <MapPinIcon class="size-4" /> {{ location }}
             </p>
-            <p><b>Price:</b> $100</p>
-            <p><b>In-stock:</b> 10</p>
+            <p>{{ description }}</p>
+            <p class=" mt-auto"><span class="font-semibold">Available slots:</span> {{ slots }}</p>
+            <p class="text-xl font-semibold"> ${{ price }}</p>
         </div>
     </div>
 </template>
